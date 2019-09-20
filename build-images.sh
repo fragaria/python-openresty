d5aeb4e197ae9d04c99147c28f3182b2c20c5402
Jirka Chadima <chadima.jiri@gmail.com>
2019-09-20 20:48:27 +0200

    Adds docker latest+version aliases

--- build-images.sh
+++ build-images.sh
@@ -4,13 +4,11 @@
 
 echo "$DOCKER_PASSWORD" | docker login --username "$DOCKER_USER" --password-stdin
 
-docker info
-
 # Build for amd64 and push
 buildctl build --frontend dockerfile.v0 \
             --local dockerfile=. \
             --local context=. \
-            --output type=image,name=docker.io/fragaria/python-openresty:test-build-amd64,push=true \
+            --output type=image,name=docker.io/fragaria/python-openresty:1.15.8-amd64,push=true \
             --opt platform=linux/amd64 \
             --opt filename=./Dockerfile
 
@@ -19,7 +17,7 @@ buildctl build --frontend dockerfile.v0 \
 buildctl build --frontend dockerfile.v0 \
             --local dockerfile=. \
             --local context=. \
-            --output type=image,name=docker.io/fragaria/python-openresty:test-build-armhf,push=true \
+            --output type=image,name=docker.io/fragaria/python-openresty:1.15.8-armhf,push=true \
             --opt platform=linux/armhf \
             --opt filename=./Dockerfile
 
@@ -27,11 +25,31 @@ buildctl build --frontend dockerfile.v0 \
 export DOCKER_CLI_EXPERIMENTAL=enabled
 
 # Create manifest list and push that
-docker manifest create fragaria/python-openresty:test-build \
-            fragaria/python-openresty:test-build-amd64 \
-            fragaria/python-openresty:test-build-armhf
+# 1.15.8
+docker manifest create fragaria/python-openresty:1.15.8 \
+            fragaria/python-openresty:1.15.8-amd64 \
+            fragaria/python-openresty:1.15.8-armhf
 
-docker manifest annotate fragaria/python-openresty:test-build fragaria/python-openresty:test-build-armhf --arch arm
-docker manifest annotate fragaria/python-openresty:test-build fragaria/python-openresty:test-build-amd64 --arch amd64
+docker manifest annotate fragaria/python-openresty:1.15.8 fragaria/python-openresty:1.15.8-armhf --arch arm
+docker manifest annotate fragaria/python-openresty:1.15.8 fragaria/python-openresty:1.15.8-amd64 --arch amd64
 
-docker manifest push fragaria/python-openresty:test-build
+docker manifest push fragaria/python-openresty:1.15.8
+
+# latest
+
+docker tag fragaria/python-openresty:1.15.8 fragaria/python-openresty:latest
+docker tag fragaria/python-openresty:1.15.8-armhf fragaria/python-openresty:latest-armhf
+docker tag fragaria/python-openresty:1.15.8-amd64 fragaria/python-openresty:latest-amd64
+
+docker push fragaria/python-openresty:latest
+docker push fragaria/python-openresty:latest-armhf
+docker push fragaria/python-openresty:latest-amd64
+
+docker manifest create fragaria/python-openresty:latest \
+            fragaria/python-openresty:latest-amd64 \
+            fragaria/python-openresty:latest-armhf
+
+docker manifest annotate fragaria/python-openresty:latest fragaria/python-openresty:latest-armhf --arch arm
+docker manifest annotate fragaria/python-openresty:latest fragaria/python-openresty:latest-amd64 --arch amd64
+
+docker manifest push fragaria/python-openresty:latest
